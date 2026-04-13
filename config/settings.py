73a61d8fa99e7ffc919b58673acf9f164497dc2e
Shaquille Babb <shaquillebabb@Shaquilles-MacBook-Pro.local>
2026-04-13 19:27:08 -0400

    feat: add custom 403, 404, 500 pages and update entrypoint to run staticfiles command

--- config/settings.py
+++ config/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [ 
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -76,6 +77,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'dashboard'
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,6 @@ done
 
 python manage.py migrate --noinput
 python manage.py seed_initial_data
+python manage.py collectstatic --noinput
 
 exec "$@"
